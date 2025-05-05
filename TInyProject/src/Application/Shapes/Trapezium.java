62debccb79e3c136d5e5e1870b244482c79ef388
hamza-vuiyan <hamza.vuiyan@gmail.com>
2025-05-05 06:00:47 +0600

    protected scopes implemented

--- TInyProject/src/Application/Shapes/Trapezium.java
+++ TInyProject/src/Application/Shapes/Trapezium.java
@@ -7,17 +7,24 @@ class Polygon {
     public Polygon(int x) {
         this.size = x;
     }
+
+    protected String colour = "RED";
+
+
+
 }
+
 
 public class Trapezium extends Polygon {
     public Trapezium(int y) { //using the super class constructor to initialize the child class
         super(y);
     }
-
     public String immediateParent() {
         return super.signature;
     } //calling the parent class signature
 
-
+    public void getColour() {
+        System.out.println("Colour: " + super.colour);
+    }
 }
 
